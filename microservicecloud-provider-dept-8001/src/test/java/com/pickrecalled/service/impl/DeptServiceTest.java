f3884cd946faffb680686d634208d73c48d74ac7
pickrecalled <pickrecalled@163.com>
2019-05-29 10:44:56 +0800

    3.3.6整合Logback

--- microservicecloud-provider-dept-8001/src/test/java/com/pickrecalled/service/impl/DeptServiceTest.java
+++ microservicecloud-provider-dept-8001/src/test/java/com/pickrecalled/service/impl/DeptServiceTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,6 +18,8 @@ import com.pickrecalled.entity.Dept;
 @SpringBootTest(classes = ProviderApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DeptServiceTest {
 
+	private static Logger logger = LoggerFactory.getLogger(DeptServiceTest.class.getName());
+
 	@Autowired
 	private DeptService deptService;
 
@@ -23,7 +27,7 @@ public class DeptServiceTest {
 	public void listTest() {
 		List<Dept> list = deptService.list();
 		for (Dept dept : list) {
-			System.out.println("dep:" + dept);
+			if (logger.isInfoEnabled()) logger.info("dep list:{}", dept);
 		}
 	}
 
