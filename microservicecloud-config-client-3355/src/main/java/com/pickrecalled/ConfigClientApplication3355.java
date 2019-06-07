35cbe392d6e9c815e05f5c0d91ad8008cc236d70
pickrecalled <pickrecalled@163.com>
2019-06-08 04:11:10 +0800

    3.SpringCloud Config客户端配置与测试

---
+++ microservicecloud-config-client-3355/src/main/java/com/pickrecalled/ConfigClientApplication3355.java
@@ -0,0 +1,13 @@
+package com.pickrecalled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication3355 {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ConfigClientApplication3355.class, args);
+	}
+
+}
