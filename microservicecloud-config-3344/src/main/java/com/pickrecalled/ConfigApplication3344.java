493fb51c8a058fec95175134a6f4a3bb0ec0283c
pickrecalled <pickrecalled@163.com>
2019-06-08 02:09:29 +0800

    2.SpringCloud Config服务端配置

---
+++ microservicecloud-config-3344/src/main/java/com/pickrecalled/ConfigApplication3344.java
@@ -0,0 +1,14 @@
+package com.pickrecalled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication3344 {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ConfigApplication3344.class, args);
+	}
+
+}
